87ac480dbd243a588f118463a643ae610c85cc74
Alex <terenteva.9529@gmail.com>
2023-11-22 23:03:16 +0200

    second task

--- script.js
+++ script.js
@@ -25,3 +25,16 @@ const personalMovieDB = {
     privat: false
 };
 
+for (let i = 0; i < 2; i++) {
+    const a = prompt('Один из последних просмотреных фильмов?', ''),
+          b = prompt('На сколько вы его оцените?', '');
+
+    if (a !=null && b !== null && a != '' && b != '' && a.length < 50) {
+        personalMovieDB.movies[a] = b;
+        console.log('done');
+    } else {
+        console.log('error');
+        i--;
+    }
+
+}
